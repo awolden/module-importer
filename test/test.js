7e5f70afcc674ab9eb9c5a8ac700a5ed5604348f
Alex WOlden <awolden@gmail.com>
2015-01-02 13:11:16 -0800

    listModules functional && some testing scaffolding

--- test/test.js
+++ test/test.js
@@ -1,5 +1,25 @@
 'use strict';
 
-var importer = require('../index');
+var Importer = require('../index');
 
 //todo: tests
+
+console.log("importting");
+
+var opts = {
+    includeLocal: false,
+    includes: [{
+        path: "./includeFolder",
+    }, {
+        path: "../node_modules/underscore",
+        direct: true
+    }]
+};
+
+var imports = new Importer(opts);
+
+if (imports.error) {
+    console.log("There was an error -> ", imports.error);
+}
+
+//console.log("require test -> ", require('./nonModuleFile'))
