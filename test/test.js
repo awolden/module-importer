bd296205332b7efaa9028eccc06cfdb60d826d6e
Alex WOlden <awolden@gmail.com>
2015-01-02 16:25:23 -0800

    added ability manually pass in a local dir

--- test/test.js
+++ test/test.js
@@ -1,24 +1,96 @@
 'use strict';
 
-var Importer = require('../index');
+/* global describe, it, before, after */
 
-//todo: tests
+var Importer = require('../index'),
+    assert = require('chai').assert,
+    expect = require('chai').expect;
 
-console.log("importting");
 
-var opts = {
-    includes: [{
-        path: "./includeFolder",
-    }, {
-        path: "../node_modules/underscore",
-        direct: true
-    }]
-};
 
-var imports = new Importer(opts);
+before(function (done) {
+    done();
+});
 
-if (imports.error) {
-    console.log("There was an error -> ", imports.error);
-}
+after(function (done) {
+    done();
+});
 
-//console.log("require test -> ", require('./nonModuleFile'))
+describe('Test Functionality', function () {
+    describe('Loads correct modules', function () {
+        it('should load all local modules', function (done) {
+
+            //provide a local path override because this file gets executed in the context
+            //of mocha
+            var imports = new Importer({
+                localPath: __dirname,
+                localFile: __filename
+            });
+
+            var expectedModules = [{
+                name: "moduleFile",
+                path: __dirname + "/module-file.js"
+            }, {
+                name: "moduleFolder",
+                path: __dirname + "/module-folder"
+            }];
+
+            expect(imports._loadedModules).to.eql(expectedModules);
+
+            done();
+        });
+        it('should load included modules', function (done) {
+
+            //provide a local path override because this file gets executed in the context
+            //of mocha
+            var imports = new Importer({
+                localPath: __dirname,
+                localFile: __filename,
+                includeLocal: false,
+                includes: [{
+                    path: "./includeFolder"
+                }]
+            });
+
+            var expectedModules = [{
+                name: "includeModuleFile",
+                path: __dirname + "/includeFolder/include-module-file.js"
+            }, {
+                name: "includeModuleFolder",
+                path: __dirname + "/includeFolder/include-module-folder"
+            }];
+
+            expect(imports._loadedModules).to.eql(expectedModules);
+
+            done();
+        });
+        it.skip('should apply filters', function (done) {
+            done();
+        });
+    });
+    describe('Modules get loaded correctly', function () {
+        it.skip('Module Values match the value from require()', function (done) {
+            done();
+        });
+    });
+});
+
+
+
+
+// var opts = {
+//     includes: [{
+//         path: "./includeFolder",
+//     }, {
+//         path: "../node_modules/underscore",
+//         direct: true
+//     }]
+// };
+//
+// var imports = new Importer(opts);
+//
+// if (imports.error) {
+//     console.log("There was an error -> ", imports.error);
+// }
+//
+// //console.log("require test -> ", require('./nonModuleFile'))
