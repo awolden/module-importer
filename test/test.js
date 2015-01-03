7920c86a0db63296e3fa3a86d474f0bbe4a5ee82
Alex WOlden <awolden@gmail.com>
2015-01-02 17:10:44 -0800

    modified filtering and added testing

--- test/test.js
+++ test/test.js
@@ -6,16 +6,6 @@ var Importer = require('../index'),
     assert = require('chai').assert,
     expect = require('chai').expect;
 
-
-
-before(function (done) {
-    done();
-});
-
-after(function (done) {
-    done();
-});
-
 describe('Test Functionality', function () {
     describe('Loads correct modules', function () {
         it('should load all local modules', function (done) {
@@ -39,7 +29,7 @@ describe('Test Functionality', function () {
 
             done();
         });
-        it('should load included modules', function (done) {
+        it('should load modules from included folder', function (done) {
 
             //provide a local path override because this file gets executed in the context
             //of mocha
@@ -64,7 +54,64 @@ describe('Test Functionality', function () {
 
             done();
         });
-        it.skip('should apply filters', function (done) {
+        it('should apply regex filter', function (done) {
+
+
+            //provide a local path override because this file gets executed in the context
+            //of mocha
+            var imports = new Importer({
+                localPath: __dirname,
+                localFile: __filename,
+                filter: /folder/g
+            });
+
+            var expectedModules = [{
+                name: "moduleFolder",
+                path: __dirname + "/module-folder"
+            }];
+
+            expect(imports._loadedModules).to.eql(expectedModules);
+
+            done();
+        });
+        it('should apply function filter', function (done) {
+
+            //provide a local path override because this file gets executed in the context
+            //of mocha
+            var imports = new Importer({
+                localPath: __dirname,
+                localFile: __filename,
+                filter: function (path) {
+                    if (path.indexOf('folder') > -1) return true
+                }
+            });
+
+            var expectedModules = [{
+                name: "moduleFolder",
+                path: __dirname + "/module-folder"
+            }];
+
+            expect(imports._loadedModules).to.eql(expectedModules);
+
+            done();
+        });
+        it('should apply string filter', function (done) {
+
+            //provide a local path override because this file gets executed in the context
+            //of mocha
+            var imports = new Importer({
+                localPath: __dirname,
+                localFile: __filename,
+                filter: "module-folder"
+            });
+
+            var expectedModules = [{
+                name: "moduleFolder",
+                path: __dirname + "/module-folder"
+            }];
+
+            expect(imports._loadedModules).to.eql(expectedModules);
+
             done();
         });
     });
